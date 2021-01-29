c804727304507a7045578efe01bd0205e2778df6
Miguel GR <68832538+mousemgr@users.noreply.github.com>
2021-01-29 12:38:51 -0600

    Initial commit

---
+++ TiposVariables.py
@@ -0,0 +1,31 @@
+#Tipos de variables en Python
+#19-Enero-2021 - MiguelGR
+
+nombre_usuario="jass" #String
+edad=39               #Int
+nombre="Miguel Gomez" #String
+premium=False         #Boolleano
+
+saldo=125.79          #Float
+genero="C"          #Caracter
+
+#Lista
+
+usuario=[nombre_usuario,edad,nombre,premium,saldo,genero]
+usuario2=["Jose",30,"Jose Emilio Pacheco",450.60,'M']
+
+numeros=[70,20,40,80,100]
+
+nombres=[]
+
+nombres.append("Juan")
+nombres.append("Jose")
+nombres.append("Julian")
+nombres.append("Jaime")
+
+
+print(usuario[2])
+print(usuario2[3])
+print(numeros[1:4])
+print(nombres[0])
+print(nombres[0:3])
